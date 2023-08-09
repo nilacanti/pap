9a532a7d5dbe60904e6fd020a353ff25e7b1f71a
nilacanti <140712997+nilacanti@users.noreply.github.com>
2023-08-09 16:17:46 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,1 +1,43 @@
-#!/bin/bash# =========================================MYIP=$(wget -qO- ipinfo.io/ip);clearapt install jq curl -y >/dev/null 2>&1read -rp "Sub Domain (Contoh: XlordVpn): " -e subDOMAIN=myvpnstoree.comSUB_DOMAIN=${sub}.myvpnstoree.comCF_ID=xlordstoreofc@gmail.comCF_KEY=a37077a98a128ab30116a6669b1ede5814cc3set -euo pipefailIP=$(wget -qO- ifconfig.me/ip);echo "Updating DNS for ${SUB_DOMAIN}..."ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \     -H "X-Auth-Email: ${CF_ID}" \     -H "X-Auth-Key: ${CF_KEY}" \     -H "Content-Type: application/json" | jq -r .result[0].id)RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \     -H "X-Auth-Email: ${CF_ID}" \     -H "X-Auth-Key: ${CF_KEY}" \     -H "Content-Type: application/json" | jq -r .result[0].id)if [[ "${#RECORD}" -le 10 ]]; then     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \     -H "X-Auth-Email: ${CF_ID}" \     -H "X-Auth-Key: ${CF_KEY}" \     -H "Content-Type: application/json" \     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)fiRESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \     -H "X-Auth-Email: ${CF_ID}" \     -H "X-Auth-Key: ${CF_KEY}" \     -H "Content-Type: application/json" \     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')echo "Host : $SUB_DOMAIN"echo "IP=" >> /var/lib/kyt/ipvps.confecho $SUB_DOMAIN > /etc/xray/domainecho $SUB_DOMAIN > /root/domainrm -f /root/cf.sh
+#!/bin/bash
+#xlordvpn
+
+# =========================================
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+apt install jq curl -y >/dev/null 2>&1
+read -rp "Sub Domain (Contoh: XlordVpn): " -e sub
+DOMAIN=myvpnstoree.com
+SUB_DOMAIN=${sub}.myvpnstoree.com
+CF_ID=xlordstoreofc@gmail.com
+CF_KEY=a37077a98a128ab30116a6669b1ede5814cc3
+set -euo pipefail
+IP=$(wget -qO- ifconfig.me/ip);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo "IP=" >> /var/lib/kyt/ipvps.conf
+echo $SUB_DOMAIN > /etc/xray/domain
+echo $SUB_DOMAIN > /root/domain
+rm -f /root/cf.sh
