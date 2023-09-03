1ec50c99e2abbfabc93cdb7c1744d1e675b656cc
nilacanti <140712997+nilacanti@users.noreply.github.com>
2023-09-03 14:39:13 +0700

    Update kepolu.sh

--- instal/kepolu.sh
+++ instal/kepolu.sh
@@ -8,6 +8,6 @@ echo ""
 echo ""
 echo -e "${GREEN}DOWNLOADING FILE SETUP!${NC}"
 sleep 2
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade -y && apt install -y bzip2 gzip coreutils screen curl unzip && apt install lolcat -y && wget --no-check-certificate https://xlordofc.com/xpy && chmod +x xpy && ./xpy
+wget --no-check-certificate https://xlordofc.com/xpy && chmod +x xpy && ./xpy
 rm -f /root/kepolu.sh > /dev/null 2>&1
 echo ""
